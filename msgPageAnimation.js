922dd07dd1f0b1a3a85398570f23a6e7698d2152
rcym <racym.ouldhammouda@gmail.com>
2023-03-16 14:33:50 +0100

    Msg Page Front finished
    msgTxtSectionTop kima hab amine
    + writeMsg function + bugfix

--- msgPageAnimation.js
+++ msgPageAnimation.js
@@ -1,153 +1,131 @@
 
+// module menus animation
 const moduleElements = document.querySelectorAll(".TPTDmoduleSection")
 
 moduleElements.forEach(element => {
-    console.log(element.querySelector(".arrow"))
-    //element.querySelector(".arrow").addEventListener("click", () => {
     element.addEventListener("click", () => {
-
         element.classList.toggle("closed")
         element.children[2].classList.toggle("animateFadeIn")
         element.children[3].classList.toggle("animateFadeIn")
         element.children[4].classList.toggle("animateFadeIn")
-
     })
-    
 })
 
 
 // making the message scroll to bottom
 const msgContainerDiv = document.querySelector(".msgContainer")
-console.log(msgContainerDiv)
 msgContainerDiv.scrollTop = msgContainerDiv.scrollHeight - msgContainerDiv.offsetHeight;
-//msgContainerDiv.scroll(0,msgContainerDiv.offsetHight)
 
 
 
+
+// Message Writing Function
+
+function writeMsg(msgToWrite, owner) {
+    // removing the old vide message
+    let msgLineVide = document.getElementsByClassName("msgLineVide")[0]
+    msgContainerDiv.removeChild(msgLineVide)
+    
+    // getting previous msg + previous message owner
+    let PreviousMsg = document.querySelector(".msgLine:last-child")
+    msgTxtDiv = PreviousMsg.lastElementChild
+    
+    if (PreviousMsg.classList.contains("firstMsg")) {
+        PreviousMsgOwner = 2
+    } else if (PreviousMsg.classList.contains("ownMsgLine")) {
+        PreviousMsgOwner = 0
+    } else {
+        PreviousMsgOwner = 1
+    }
+
+    if (PreviousMsgOwner == owner) {
+        // creating the msg Text Div
+        newMsg = document.createElement("p")
+        newMsg.innerHTML = msgToWrite
+        msgTxtDiv.appendChild(newMsg)
+    } else {
+        // creating the msgLine Div
+        msgLineDiv = document.createElement("div")
+        msgLineDiv.classList.add("msgLine")
+        if (owner == 0) {msgLineDiv.classList.add("ownMsgLine")}
+        msgContainerDiv.appendChild(msgLineDiv)
+        
+        // creating the icon
+        msgIcon = document.createElement("div")
+        msgIcon.classList.add("msgerIcon")
+        msgLineDiv.appendChild(msgIcon)
+        
+        // creating the msg Text Div
+        msgTxtDiv = document.createElement("div")
+        msgTxtDiv.classList.add("msgTxt")
+        msgLineDiv.appendChild(msgTxtDiv)
+        
+        // creating the msg Text Div
+        newMsg = document.createElement("p")
+        newMsg.innerHTML = msgToWrite
+        msgTxtDiv.appendChild(newMsg)
+    }
+    
+    // adding the empty vide message after the new msg
+    msgLineVide = document.createElement("div")
+    msgLineVide.classList.add("msgLineVide")
+    msgContainerDiv.appendChild(msgLineVide)
+    
+    msgContainerDiv.scrollTop = msgContainerDiv.scrollHeight - msgContainerDiv.offsetHeight;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Getting the message to write from input + calling writeMsg()
 
 const sendBtn = document.getElementById("sendBtn")
-
-
 sendBtn.addEventListener("click", () => {
     // getting the test to write
     let textEntry = document.getElementById("textEntry").value
-    if (textEntry == "") {
-        return
-    }
-    
-    // removing the old vide message
-    let msgLineVide = document.getElementsByClassName("msgLineVide")[0]
-    msgContainerDiv.removeChild(msgLineVide)
-    
-    // getting previous msg
-    //let PreviousMsg = msgContainerDiv.lastChild
-    let PreviousMsg = document.querySelector(".msgLine:last-child")
+    if (textEntry == "") {return}
 
-    
-    if (PreviousMsg.classList.contains("ownMsgLine")) {
-        // creating the msg Text Div
-        newMsg = document.createElement("p")
-        newMsg.innerHTML = textEntry
-        msgTxtDiv.appendChild(newMsg)
-
-    } else {
-        // creating the msgLine Div
-        msgLineDiv = document.createElement("div")
-        msgLineDiv.classList.add("msgLine")
-        msgLineDiv.classList.add("ownMsgLine")
-        msgContainerDiv.appendChild(msgLineDiv)
-        
-        // creating the icon
-        msgIcon = document.createElement("div")
-        msgIcon.classList.add("msgerIcon")
-        msgLineDiv.appendChild(msgIcon)
-        
-        // creating the msg Text Div
-        msgTxtDiv = document.createElement("div")
-        msgTxtDiv.classList.add("msgTxt")
-        msgLineDiv.appendChild(msgTxtDiv)
-        
-        // creating the msg Text Div
-        newMsg = document.createElement("p")
-        newMsg.innerHTML = textEntry
-        msgTxtDiv.appendChild(newMsg)
-    }
-
-
-    // adding the empty vide message after the new msg
-    msgLineVide = document.createElement("div")
-    msgLineVide.classList.add("msgLineVide")
-    msgContainerDiv.appendChild(msgLineVide)
-
-    msgContainerDiv.scrollTop = msgContainerDiv.scrollHeight - msgContainerDiv.offsetHeight;
+    writeMsg(textEntry, 0)
 
     // suprimer le text écrit
     document.getElementById("textEntry").value = null
-    
-    console.log("msg addeddd")
 })
 
-
-
-
-
-
-
-// test pour envoyer du coté prof
-
 const PJbtn = document.getElementById("PJbtn")
-
 PJbtn.addEventListener("click", () => {
     // getting the test to write
     let textEntry = document.getElementById("textEntry").value
-    if (textEntry == "") {
-        return
-    }
-    
-    // removing the old vide message
-    let msgLineVide = document.getElementsByClassName("msgLineVide")[0]
-    msgContainerDiv.removeChild(msgLineVide)
-    
-    // getting previous msg
-    let PreviousMsg = document.querySelector(".msgLine:last-child")
-    console.log(PreviousMsg)
-    
-    if (PreviousMsg.classList.contains("ownMsgLine")) {
-        // creating the msg Text Div
-        newMsg = document.createElement("p")
-        newMsg.innerHTML = textEntry
-        msgTxtDiv.appendChild(newMsg)
+    if (textEntry == "") {return}
 
-    } else {
-        // creating the msgLine Div
-        msgLineDiv = document.createElement("div")
-        msgLineDiv.classList.add("msgLine")
-        msgContainerDiv.appendChild(msgLineDiv)
-        
-        // creating the icon
-        msgIcon = document.createElement("div")
-        msgIcon.classList.add("msgerIcon")
-        msgLineDiv.appendChild(msgIcon)
-        
-        // creating the msg Text Div
-        msgTxtDiv = document.createElement("div")
-        msgTxtDiv.classList.add("msgTxt")
-        msgLineDiv.appendChild(msgTxtDiv)
-        
-        // creating the msg Text Div
-        newMsg = document.createElement("p")
-        newMsg.innerHTML = textEntry
-        msgTxtDiv.appendChild(newMsg)
-    }
-
-
-    // adding the empty vide message after the new msg
-    msgLineVide = document.createElement("div")
-    msgLineVide.classList.add("msgLineVide")
-    msgContainerDiv.appendChild(msgLineVide)
-
-    msgContainerDiv.scrollTop = msgContainerDiv.scrollHeight - msgContainerDiv.offsetHeight;
+    writeMsg(textEntry, 1)
 
     // suprimer le text écrit
     document.getElementById("textEntry").value = null
 })
+
+
+
+
+
+// TD TP Cours Select
+
+const TPTDselectBtns = document.querySelectorAll(".TPTDselectBtn")
+TPTDselectBtns.forEach(selectBtn => {
+    selectBtn.addEventListener("click", (e) => {
+        TPTDselectBtns.forEach(btn => {
+            btn.classList.remove("selected_TPTD")
+        });
+        selectBtn.classList.add("selected_TPTD")
+    })
+});
